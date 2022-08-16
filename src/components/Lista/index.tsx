433ee7a3fc80d618a4e1ea46daf0c4d90611b020
Gabriel Amaral <gabriel99979@gmail.com>
2022-08-16 16:51:51 -0300

    Sexta parte do curso

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,17 +2,22 @@ import style from "./Lista.module.scss"
 import Item from "./item";
 import {ITarefa } from "../../Types/Tarefas";
 
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 
-function Lista({tarefas} : {tarefas: ITarefa[]}){
+
+function Lista({tarefas,selecionaTarefa} : Props){
     
-
     return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) => 
-                (
-                    <Item key={index}
+                {tarefas.map(item => ( 
+                    <Item 
+                    key={item.id}
+                    selecionaTarefa={selecionaTarefa}
                     {...item}
                     />
                 )
